c08c7c7c317278c048b379ff9f8a0fd53642c4e6
Ahmad Mursyidan <ahmadmursyidan201@gmail.com>
2022-11-10 07:00:37 +0700

    implement e2e testing for liking & unliking the restaurant

---
+++ e2e/Liking_Restaurants.spec.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('#/favorites');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('.restaurant-is-empty');
+  I.see('Nothing to see here :(', 'p');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.seeElement('.restaurant-is-empty');
+  I.see('Nothing to see here :(', 'p');
+  I.amOnPage('');
+
+  I.waitForElement('.restaurant-item__name a', 10);
+  I.seeElement('.restaurant-item__name a');
+  const firstRestaurant = locate('.restaurant-item__name a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.waitForElement('#likeButton', 10);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('#/favorites');
+  I.waitForElement('.restaurant-item', 10);
+  I.seeElement('.restaurant-item');
+  const likedRestaurantTitle = await I.grabTextFrom('.restaurant-item__name a');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+});
